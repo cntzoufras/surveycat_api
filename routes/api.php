c1a1efb0743a72fd39a279c537df0ebfcff0f267
Constantinos Ntzoufras <cntzoufras@gmail.com>
2022-09-24 21:27:33 +0300

    [feat] Fix routes

--- routes/api.php
+++ routes/api.php
@@ -19,39 +19,31 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/**
-** Basic Routes for a RESTful service:
-**
-** Route::get($uri, $callback);
-** Route::post($uri, $callback);
-** Route::put($uri, $callback);
-** Route::delete($uri, $callback);
-**
-**/
- 
+// Products
 Route::get('products', function () {
     return response(['Product 1', 'Product 2', 'Product 3'],200);
 });
- 
 Route::get('products/{product}', function ($productId) {
     return response()->json(['productId' => "{$productId}"], 200);
 });
-  
- 
 Route::post('products', function() {
     return  response()->json([
             'message' => 'Create success'
         ], 201);
 });
-
-Route::get('posts',[PostController::class,'getPost']);
-
 Route::put('products/{product}', function() {
     return  response()->json([
             'message' => 'Update success'
         ], 200);
 });
- 
 Route::delete('products/{product}',function() {
     return  response()->json(null, 204);
 });
+
+// Posts
+Route::get('/add-post', [PostController::class,'addPost']);
+Route::post('/create-post',[PostController::class,'createPost'])->name('post.create');
+Route::get('/posts/{id}', [PostController::class,'getPostById'])->name('post.get');
+Route::get('/delete-post/{id}',[PostController::class,'deletePost'])->name('post.delete');
+Route::get('/edit-post/{id}', [PostController::class,'editPost'])->name('post.edit');
+Route::post('/update-post',[PostController::class,'updatePost'])->name('post.update');
--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', function () {
-    return view('index');
-});
-
-//Route::get('/add-post', [PostController::class,'addPost']);
-//
-//Route::post('/create-post',[PostController::class,'createPost'])->name('post.create');
-//
-//
-//Route::get('/posts/{id}', [PostController::class,'getPostById']);
-//
-//Route::get('/delete-post/{id}',[PostController::class,'deletePost']);
-//
-//Route::get('/edit-post/{id}', [PostController::class,'editPost']);
-//
-//Route::post('/update-post',[PostController::class,'updatePost'])->name('post.update');
+ Route::get('/', function () {
+     return view('welcome');
+ });
