b8d505b4a9f009a8b9062fc9fc3d05bef5b424dc
Constantinos Ntzoufras <cntzoufras@gmail.com>
2022-10-29 01:54:04 +0300

    [feat] Adds testing posts view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,26 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>All Posts</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
-	<section style="padding-top: 60px;">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="card">
-						<div class="card-header">
-							All Posts
-						</div>
-						<div class="card-body">
-							{{ $posts ?? '' }}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+<section style="padding-top: 60px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        All Posts
+                    </div>
+                    <div class="card-body">
+                        <table>
+                            <tr>
+                                <th>ID</th>
+                                <th>TITLE</th>
+                                <th>BODY</th>
+                                <th>CREATED_AT</th>
+                                <th>UPDATED_AT</th>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+                            </tr>
+                            @foreach($posts as $key => $data)
+
+                                <tr>
+                                    <td>{{$data->id}}</td>
+                                    <td>{{$data->title}}</td>
+                                    <td>{{$data->body}}</td>
+                                    <td>{{$data->created_at}}</td>
+                                    <td>{{$data->updated_at}}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+                        {{--                        {{ $posts ?? '' }}<br/><br/>--}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
- Route::get('/', function () {
-     return view('welcome');
- });
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/posts', function () {
+    $data = new Post();
+    $posts = Post::where('id', '>=', 1)->where('id', '<', 20)->get();
+    return view('posts', ['posts' => $posts]);
+});
