d3c9114e7db545bcd19b0f5d36beb8dc6d5f0411
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-02-27 19:48:04 +0200

    Update api & web

--- routes/api.php
+++ routes/api.php
@@ -1,69 +1,74 @@
 <?php
+    
+    use App\Http\Resources\PostCollection;
+    use App\Http\Controllers\VesselController;
+    use App\Http\Resources\PostResource;
+    use App\Models\Post;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
+    
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | is assigned the "api" middleware group. Enjoy building your API!
+    |
+    */
 
-use App\Http\Controllers\PostController;
-use App\Http\Resources\PostCollection;
-use App\Http\Resources\PostResource;
-use App\Models\Post;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+//    Route::group(['middleware' => ['authentication'], 'namespace' => 'Auth'], function () {
+//        Route::post('login', 'AuthenticationController@login');
+//        Route::post('logout', 'AuthenticationController@logout');
+//        Route::post('register', 'AuthenticationController@register');
+//        Route::post('refresh', 'AuthenticationController@refresh');
+//        Route::post('forgot-password', 'AuthenticationController@forgotPassword');
+//        Route::post('reset-password', 'AuthenticationController@resetPassword');
+//    });
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::group(['middleware' => ['aris'], 'namespace' => 'Auth'], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('register', 'AuthController@register');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('forgot-password', 'AuthController@forgotPassword');
-    Route::post('reset-password', 'AuthController@resetPassword');
-});
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//    Route::middleware('auth:api')->get('/user', function (Request $request) {
+//        return $request->user();
+//    });
 // Resources
-Route::get('/posts/{id}', function ($id) {
-    return new PostResource(Post::findOrFail($id));
-});
-
-Route::get('/posts', function () {
-    return PostResource::collection(Post::all());
-});
-
-//Route::get('/posts', function () {
-//    return new PostResource(Post::query()->where('id', '<', 10));
-//});
-Route::get('/posts-collection', function () {
-    return new PostCollection(Post::paginate());
-});
-
-// --
-// Products
-Route::get('/products/{product}', function ($id) {
-    return response()->json(['productId' => "{$id}"], 201);
-});
-
-Route::get('/products', function () {
-    return response()->json([
-        'message' => 'msg: Products',
-    ], 201);
-});
-
-Route::put('/products/{product}', function () {
-    return response()->json([
-        'message' => 'Update success',
-    ], 200);
-});
-
-Route::delete('/products/{product}', function () {
-    return response()->json(null, 204);
-});
+    Route::get('/posts/{id}', function ($id) {
+        return new PostResource(Post::findOrFail($id));
+    });
+    
+    Route::get('/posts', function () {
+        return PostResource::collection(Post::all());
+    });
+    
+    Route::post('/posts', 'App\Http\Controllers\PostController@store');
+    Route::post('/vessels', [VesselController::class, 'store']);
+    Route::get('/vessels', [VesselController::class, 'index']);
+    Route::get('/vessels/{id}', [VesselController::class, 'show']);
+    
+    Route::get('/posts', function () {
+        return new PostResource(Post::query()->where('id', '<', 10));
+    });
+    Route::get('/posts-collection', function () {
+        return new PostCollection(Post::paginate());
+    });
+    
+    // --
+    // Products
+    Route::get('/products/{product}', function ($id) {
+        return response()->json(['productId' => "{$id}"], 201);
+    });
+    
+    Route::get('/products', function () {
+        return response()->json([
+            'message' => 'msg: Products',
+        ], 201);
+    });
+    
+    Route::put('/products/{product}', function () {
+        return response()->json([
+            'message' => 'Update success',
+        ], 200);
+    });
+    
+    Route::delete('/products/{product}', function () {
+        return response()->json(null, 204);
+    });
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,18 @@
 <?php
-
-use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/posts', function () {
-    $data = new Post();
-    $posts = Post::where('id', '>=', 1)->where('id', '<', 20)->get();
-    return view('posts', ['posts' => $posts]);
-});
+    
+    use Illuminate\Support\Facades\Route;
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
+    
+    Route::get('/', function () {
+        return view('welcome');
+    });
