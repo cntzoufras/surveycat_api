493d307acffa8c4eea4df54f307c0ab523c78ff9
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-02-27 20:17:15 +0200

    Update PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,44 +1,52 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use App\Http\Resources\PostCollection;
-use App\Http\Resources\PostResource;
-use Illuminate\Http\Request;
-use App\Models\Post;
-
-class PostController extends Controller {
-
-    public function index() {
-        return new PostCollection(Post::all());
+    
+    namespace App\Http\Controllers;
+    
+    use App\Http\Resources\PostCollection;
+    use App\Http\Resources\PostResource;
+    use Illuminate\Http\Request;
+    use App\Models\Post;
+    
+    class PostController extends Controller {
+        
+        public function index() {
+            return new PostCollection(Post::all());
+        }
+        
+        public function show($id) {
+            $post = Post::find($id);
+            return new PostResource($post);
+        }
+        
+        public function store(Request $request) {
+            
+            $validated = $request->validate([
+                'title'       => 'required|string|max:255',
+                'description' => 'required|string',
+            ]);
+            $post = new Post;
+            $post->title = $request->input('title');
+            $post->description = $request->input('description');
+            $post->fill($validated);
+            $post->save();
+//            return response()->json($post);
+            return response()->json(['title'   => $post->title, 'body' => $post->description,
+                                     'message' => 'Post created',
+            ], 201);
+        }
+        
+        public function update(Request $request) {
+            $post = Post::find($request->id);
+            $post->title = $request->title;
+            $post->body = $request->body;
+            $post->save();
+            return back()->with('post_updated', 'Post has been updated successfully!');
+        }
+        
+        public function deletePost($id) {
+            Post::where('id', $id)->delete();
+            return back()->with('post_deleted', 'Post has been deleted successfully!');
+        }
+        
+        
     }
-
-    public function show($id) {
-        $post = Post::find($id);
-        return new PostResource($post);
-    }
-
-    public function create(Request $request) {
-        $post = new Post();
-        $post->title = $request->title;
-        $post->body = $request->body;
-
-        $post->save();
-        return response()->json(['title' => $post->title, 'body' => $post->body]);
-    }
-
-    public function update(Request $request) {
-        $post = Post::find($request->id);
-        $post->title = $request->title;
-        $post->body = $request->body;
-        $post->save();
-        return back()->with('post_updated', 'Post has been updated successfully!');
-    }
-
-    public function deletePost($id) {
-        Post::where('id', $id)->delete();
-        return back()->with('post_deleted', 'Post has been deleted successfully!');
-    }
-
-
-}
