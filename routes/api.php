a21d8dfd2dbbee2d23f454138c5064102d27f306
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-03-26 19:29:01 +0300

    Update for AuthenticationController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
     
+    use App\Http\Controllers\Auth\AuthenticationController;
     use App\Http\Controllers\SurveyPagesController;
     use App\Http\Resources\PostCollection;
-    use App\Http\Controllers\VesselController;
+    use App\Http\Controllers\SurveyTemplateController;
     use App\Http\Controllers\QuestionController;
     use App\Http\Resources\PostResource;
     use App\Models\Post;
@@ -19,55 +20,49 @@
     | is assigned the "api" middleware group. Enjoy building your API!
     |
     */
+    
+    Route::controller(AuthenticationController::class)->group(function () {
+        
+        Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
+        Route::post('logout', [AuthenticationController::class, 'logout']);
+        Route::post('register', [AuthenticationController::class, 'register']);
+        Route::post('refresh', [AuthenticationController::class, 'refresh']);
+        Route::post('forgot-password', [AuthenticationController::class, 'forgot-password']);
+        Route::post('reset-password', [AuthenticationController::class, 'reset-password']);
+    });
 
-//    Route::group(['middleware' => ['authentication'], 'namespace' => 'Auth'], function () {
-//        Route::post('login', 'AuthenticationController@login');
-//        Route::post('logout', 'AuthenticationController@logout');
-//        Route::post('register', 'AuthenticationController@register');
-//        Route::post('refresh', 'AuthenticationController@refresh');
-//        Route::post('forgot-password', 'AuthenticationController@forgotPassword');
-//        Route::post('reset-password', 'AuthenticationController@resetPassword');
-//    });
-
-//    Route::middleware('auth:api')->get('/user', function (Request $request) {
-//        return $request->user();
-//    });
-// Resources
+//    Route::middleware('auth:sanctum')->group(function () {
     Route::get('/posts/{id}', function ($id) {
         return new PostResource(Post::findOrFail($id));
     });
-    
     Route::get('/posts', function () {
         return PostResource::collection(Post::all());
     });
-    
     Route::get('/posts-less-than-id-ten', function () {
         return new PostResource(Post::query()->where('id', '<', 10));
     });
-    
+    Route::prefix('SurveyTemplates')->group(function () {
+        Route::get('/', [SurveyTemplateController::class, 'index']);
+        Route::post('/', [SurveyTemplateController::class, 'store']);
+        Route::get('/{id}', [SurveyTemplateController::class, 'show']);
+    });
+    Route::prefix('questions')->group(function () {
+        Route::get('/', [QuestionController::class, 'index']);
+        Route::post('/', [QuestionController::class, 'store']);
+        Route::get('/{id}', [QuestionController::class, 'show']);
+    });
+    Route::prefix('survey-pages')->group(function () {
+        Route::get('/', [SurveyPagesController::class, 'index']);
+        Route::post('/', [SurveyPagesController::class, 'store']);
+        Route::get('/{id}', [SurveyPagesController::class, 'show']);
+    });
     Route::get('/posts-collection', function () {
         return new PostCollection(Post::paginate());
     });
     
     Route::post('/posts', 'App\Http\Controllers\PostController@store');
     
-    Route::prefix('vessels')->group(function () {
-        Route::get('/', [VesselController::class, 'index']);
-        Route::post('/', [VesselController::class, 'store']);
-        Route::get('/{id}', [VesselController::class, 'show']);
-    });
-    
-    Route::prefix('questions')->group(function () {
-        Route::get('/', [QuestionController::class, 'index']);
-        Route::post('/', [QuestionController::class, 'store']);
-        Route::get('/{id}', [QuestionController::class, 'show']);
-    });
-    
-    Route::prefix('survey-pages')->group(function () {
-        Route::get('/', [SurveyPagesController::class, 'index']);
-        Route::post('/', [SurveyPagesController::class, 'store']);
-        Route::get('/{id}', [SurveyPagesController::class, 'show']);
-    });
+    // Functionality
     
     // Products
     Route::get('/products/{product}', function ($id) {
@@ -89,3 +84,10 @@
     Route::delete('/products/{product}', function () {
         return response()->json(null, 204);
     });
+    //    });
+    
+    //    Route::middleware('auth:api')->get('/user', function (Request $request) {
+    //        return $request->user();
+    //    });
+    
+
