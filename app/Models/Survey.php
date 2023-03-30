285716b95ae59cc68a273b0e89f078f3bf2b57ad
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-03-30 22:42:31 +0300

    Add Survey mfrcs

---
+++ app/Models/Survey.php
@@ -0,0 +1,19 @@
+<?php
+    
+    namespace App\Models;
+    
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
+    
+    class Survey extends Model {
+        
+        use HasFactory;
+        
+        public function survey_styles() {
+            return $this->belongsToMany(SurveyStyle::class);
+        }
+        
+        public function survey_pages() {
+            return $this->belongsToMany(SurveyPage::class);
+        }
+    }
--- app/Models/SurveyStyle.php
+++ app/Models/SurveyStyle.php
@@ -1,11 +1,21 @@
 <?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class SurveyStyle extends Model
-{
-    use HasFactory;
-}
+    
+    namespace App\Models;
+    
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
+    
+    class SurveyStyle extends Model {
+        
+        use HasFactory;
+        
+        protected $table = 'survey_styles';
+        
+        protected $fillable = ['name', 'description', 'style_options'];
+        
+        protected $guarded = [''];
+        
+        public function surveys() {
+            return $this->belongsToMany(Survey::class);
+        }
+    }
