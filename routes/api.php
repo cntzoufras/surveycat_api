59f772b02efca31a34d86a0e0ad9a6aa360cab0f
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-10-15 00:07:19 +0300

    Cleanup leftovers

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,6 @@
     Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
         return $request->user();
     });
-    
     Route::controller(AuthenticationController::class)->group(function () {
         
         Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
@@ -33,17 +32,6 @@
         Route::post('refresh', [AuthenticationController::class, 'refresh']);
         Route::post('forgot-password', [AuthenticationController::class, 'forgot-password']);
         Route::post('reset-password', [AuthenticationController::class, 'reset-password']);
-    });
-
-//    Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/posts/{id}', function ($id) {
-        return new PostResource(Post::findOrFail($id));
-    });
-    Route::get('/posts', function () {
-        return PostResource::collection(Post::all());
-    });
-    Route::get('/posts-less-than-id-ten', function () {
-        return new PostResource(Post::query()->where('id', '<', 10));
     });
     Route::prefix('survey-templates')->group(function () {
         Route::get('/', [SurveyTemplateController::class, 'index']);
@@ -66,38 +54,3 @@
         Route::post('/', [SurveySubmissionController::class, 'store']);
         Route::get('/{id}', [SurveySubmissionController::class, 'show']);
     });
-    Route::get('/posts-collection', function () {
-        return new PostCollection(Post::paginate());
-    });
-    
-    Route::post('/posts', 'App\Http\Controllers\PostController@store');
-    
-    // Functionality
-    
-    // Products
-    Route::get('/products/{product}', function ($id) {
-        return response()->json(['productId' => "{$id}"], 201);
-    });
-    
-    Route::get('/products', function () {
-        return response()->json([
-            'message' => 'msg: Products',
-        ], 201);
-    });
-    
-    Route::put('/products/{product}', function () {
-        return response()->json([
-            'message' => 'Update success',
-        ], 200);
-    });
-    
-    Route::delete('/products/{product}', function () {
-        return response()->json(null, 204);
-    });
-    //    });
-    
-    //    Route::middleware('auth:api')->get('/user', function (Request $request) {
-    //        return $request->user();
-    //    });
-    
-
