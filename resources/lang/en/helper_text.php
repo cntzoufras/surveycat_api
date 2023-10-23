e3154b94a6e60658c46a30fabf8b294e87a2fe16
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-10-23 23:40:40 +0300

    Add helper_text file

---
+++ resources/lang/en/helper_text.php
@@ -0,0 +1,25 @@
+<?php
+    return [
+        
+        /*
+        |--------------------------------------------------------------------------
+        | Helper Language Lines
+        |--------------------------------------------------------------------------
+        |
+        | The following language lines are used by the paginator library to build
+        | the simple pagination links. You are free to change them to anything
+        | you want to customize your views to better match your application.
+        |
+        */
+        
+        'survey_format' => [
+            'one'     => ['title'       => 'One Question at a Time',
+                          'description' => 'Automatically scroll to the next question. Best for shorter, one-page surveys. People can focus their attention on one question at a time before moving on to the next one. Works with all question types.',
+            ],
+            'classic' => ['title'       => 'Classic',
+                          'description' => 'Show all questions on a page at once. Best for longer surveys with multiple pages. People can see how much they have to answer at a glance. Works with all question types.',
+            ],
+        ],
+        'test'          => 'test',
+    
+    ];
