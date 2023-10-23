8b45ef6cf9514bbec3ee247db2fbd759c1a36653
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-10-23 23:31:03 +0300

    Update api routes after draft init batch

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
     
     use App\Http\Controllers\Auth\AuthenticationController;
     use App\Http\Controllers\SurveyCategoryController;
+    use App\Http\Controllers\SurveyPageController;
+    use App\Http\Controllers\SurveyRespondentController;
     use App\Http\Controllers\SurveySubmissionController;
     use App\Http\Controllers\SurveyTemplateController;
     use App\Http\Controllers\QuestionController;
+    use App\Http\Controllers\ThemeSettingsController;
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Route;
     
@@ -19,10 +22,10 @@
     |
     */
     Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+        
         return $request->user();
     });
     Route::controller(AuthenticationController::class)->group(function () {
-        
         Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
         Route::post('logout', [AuthenticationController::class, 'logout']);
         Route::post('register', [AuthenticationController::class, 'register']);
@@ -35,19 +38,40 @@
         Route::post('/', [SurveyTemplateController::class, 'store']);
         Route::get('/{id}', [SurveyTemplateController::class, 'show']);
     });
-    Route::prefix('survey-categories')->group(function () {
-        Route::get('/', [SurveyCategoryController::class, 'index']);
-        Route::post('/', [SurveyCategoryController::class, 'store']);
-        Route::get('/{id}', [SurveyCategoryController::class, 'show']);
-        Route::delete('/{id}', [SurveyCategoryController::class, 'delete']);
-    });
     Route::prefix('questions')->group(function () {
         Route::get('/', [QuestionController::class, 'index']);
         Route::post('/', [QuestionController::class, 'store']);
+        Route::put('/{id}', [QuestionController::class, 'update']);
         Route::get('/{id}', [QuestionController::class, 'show']);
+        Route::delete('/{id}', [QuestionController::class, 'delete']);
     });
     Route::prefix('survey-pages')->group(function () {
-        Route::get('/', [SurveySubmissionController::class, 'index']);
-        Route::post('/', [SurveySubmissionController::class, 'store']);
-        Route::get('/{id}', [SurveySubmissionController::class, 'show']);
+        Route::get('/', [SurveyPageController::class, 'index']);
+        Route::post('/', [SurveyPageController::class, 'store']);
+        Route::put('/{id}', [SurveyPageController::class, 'update']);
+        Route::get('/{id}', [SurveyPageController::class, 'show']);
+        Route::delete('/{id}', [SurveyPageController::class, 'delete']);
     });
+    Route::prefix('theme-styles')->group(function () {
+        Route::get('/', [ThemeSettingsController::class, 'index']);
+        Route::post('/', [ThemeSettingsController::class, 'store']);
+        Route::put('/', [ThemeSettingsController::class, 'update']);
+        Route::get('/{id}', [ThemeSettingsController::class, 'show']);
+        Route::delete('/{id}', [ThemeSettingsController::class, 'delete']);
+    });
+    Route::prefix('survey-respondents')->group(function () {
+        Route::get('/', [SurveyRespondentController::class, 'index']);
+        Route::post('/', [SurveyRespondentController::class, 'store']);
+        Route::put('/', [SurveyRespondentController::class, 'update']);
+        Route::get('/{id}', [SurveyRespondentController::class, 'show']);
+        Route::delete('/{id}', [SurveyRespondentController::class, 'delete']);
+    });
+    
+    Route::prefix('survey-categories')->group(function () {
+        Route::get('/', [SurveyCategoryController::class, 'index']);
+        Route::post('/', [SurveyCategoryController::class, 'store']);
+        Route::put('/{id}', [SurveyCategoryController::class, 'update']);
+        Route::get('/{id}', [SurveyCategoryController::class, 'show']);
+        Route::delete('/{id}', [SurveyCategoryController::class, 'delete']);
+    });
+    
