3babca336a892b759ac6b22df6278a0fce4c26a3
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-10-25 00:03:30 +0300

    Refactor SurveyQuestionOption to SurveyQuestionChoice

---
+++ app/Policies/SurveyQuestionChoicePolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\SurveyQuestionChoice;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class SurveyQuestionChoicePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, SurveyQuestionChoice $surveyQuestionChoice): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, SurveyQuestionChoice $surveyQuestionChoice): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, SurveyQuestionChoice $surveyQuestionChoice): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, SurveyQuestionChoice $surveyQuestionChoice): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, SurveyQuestionChoice $surveyQuestionChoice): bool
+    {
+        //
+    }
+}
