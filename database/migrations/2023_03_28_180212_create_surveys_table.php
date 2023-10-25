508bf0f4b8ffee3ab899822aaa0f7aedc135b951
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-10-25 23:58:12 +0300

    [chore] Remove leftovers

--- database/migrations/2023_03_28_180212_create_surveys_table.php
+++ database/migrations/2023_03_28_180212_create_surveys_table.php
@@ -1,8 +1,5 @@
 <?php
     
-    use App\Models\SurveyCategory;
-    use App\Models\SurveyQuestion;
-    use App\Models\SurveyStatus;
     use Illuminate\Database\Migrations\Migration;
     use Illuminate\Database\Schema\Blueprint;
     use Illuminate\Support\Facades\Schema;
