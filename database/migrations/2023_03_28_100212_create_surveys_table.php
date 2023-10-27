d06d33e81ffd8c4f2f1511e9bd8fe56d63658c8c
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-10-28 01:51:18 +0300

    Add is_template + template in survey

--- database/migrations/2023_03_28_100212_create_surveys_table.php
+++ database/migrations/2023_03_28_100212_create_surveys_table.php
@@ -3,7 +3,6 @@
     use Illuminate\Database\Migrations\Migration;
     use Illuminate\Database\Schema\Blueprint;
     use Illuminate\Support\Facades\Schema;
-    use Illuminate\Support\Str;
     
     return new class extends Migration {
         
@@ -24,8 +23,10 @@
                 $table->string('banner_image')->nullable()->default(null);
                 $table->bigInteger('views')->default(0);
                 $table->timestamps();
+                $table->boolean('is_template');
                 $table->foreignId('survey_category_id')->constrained('survey_categories');
                 $table->foreignId('survey_status_id')->constrained('survey_statuses');
+                $table->uuid('survey_template_id')->nullable();
             });
         }
         
