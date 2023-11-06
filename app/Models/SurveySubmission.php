ef47f853657ece0e6507d6b071fcf0e1a46113d2
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-07 00:17:28 +0200

    Update relation to Respondent

--- app/Models/SurveySubmission.php
+++ app/Models/SurveySubmission.php
@@ -5,6 +5,7 @@
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
     use Illuminate\Database\Eloquent\Relations\BelongsTo;
+    use Illuminate\Database\Eloquent\Relations\HasMany;
     
     class SurveySubmission extends Model {
         
@@ -18,7 +19,11 @@
             return $this->belongsTo(Survey::class);
         }
         
-        public function survey_respondent(): BelongsTo {
-            return $this->belongsTo(SurveyRespondent::class);
+        public function respondent(): BelongsTo {
+            return $this->belongsTo(Respondent::class);
+        }
+        
+        public function survey_questions(): HasMany {
+            return $this->hasMany(SurveyQuestion::class);
         }
     }
