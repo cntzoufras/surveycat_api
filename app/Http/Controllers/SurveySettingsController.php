f21c467a75d09bf2485a4331739490f0fcc020b9
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-07 00:16:49 +0200

    Add SurveySettings to keep options like show page title/numbers etc

---
+++ app/Http/Controllers/SurveySettingsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSurveySettingsRequest;
+use App\Http\Requests\UpdateSurveySettingsRequest;
+use App\Models\SurveySettings;
+
+class SurveySettingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSurveySettingsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SurveySettings $surveySettings)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SurveySettings $surveySettings)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSurveySettingsRequest $request, SurveySettings $surveySettings)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SurveySettings $surveySettings)
+    {
+        //
+    }
+}
