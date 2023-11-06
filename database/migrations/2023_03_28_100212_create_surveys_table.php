1adc5db8eb5272a8dc75a82e2fb577a42ec2fb73
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-07 00:24:23 +0200

    Refactor Surveys migration, use settings, & responses, keep only as in monkey

--- database/migrations/2023_03_28_100212_create_surveys_table.php
+++ database/migrations/2023_03_28_100212_create_surveys_table.php
@@ -11,22 +11,14 @@
          */
         public function up(): void {
             Schema::create('surveys', function (Blueprint $table) {
-                $table->uuid('id')->primary()->index();
+                $table->uuid('id')->primary()->unique();
                 $table->string('title');
-                $table->string('page_title');
-                $table->boolean('show_page_title')->default(true);
-                $table->boolean('show_page_numbers')->default(true);
-                $table->boolean('show_question_numbers')->default(true);
-                $table->boolean('show_progress_bar')->default(false);
-                $table->boolean('required_asterisks')->default(true);
-                $table->string('public_link')->nullable()->default(null);
-                $table->string('banner_image')->nullable()->default(null);
-                $table->bigInteger('views')->default(0);
+                $table->string('description');
                 $table->timestamps();
-                $table->boolean('is_template');
+                $table->integer('views')->default(0);
                 $table->foreignId('survey_category_id')->constrained('survey_categories');
                 $table->foreignId('survey_status_id')->constrained('survey_statuses');
-                $table->uuid('survey_template_id')->nullable();
+                $table->index(['id', 'title']);
             });
         }
         
