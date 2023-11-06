73cb24f696afaa2c6289bec66c897eba230fbf0a
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-07 00:13:56 +0200

    Add SurveyResponse to keep Survey response metadata

---
+++ app/Http/Controllers/SurveyResponseController.php
@@ -0,0 +1,59 @@
+<?php
+    
+    namespace App\Http\Controllers;
+    
+    use App\Http\Requests\StoreSurveyResponseRequest;
+    use App\Http\Requests\UpdateSurveyResponseRequest;
+    use App\Models\Response;
+    
+    class SurveyResponseController extends Controller {
+        
+        /**
+         * Display a listing of the resource.
+         */
+        public function index() {
+            //
+        }
+        
+        /**
+         * Show the form for creating a new resource.
+         */
+        public function create() {
+            //
+        }
+        
+        /**
+         * Store a newly created resource in storage.
+         */
+        public function store(StoreSurveyResponseRequest $request) {
+            //
+        }
+        
+        /**
+         * Display the specified resource.
+         */
+        public function show(Response $surveyResponse) {
+            //
+        }
+        
+        /**
+         * Show the form for editing the specified resource.
+         */
+        public function edit(Response $surveyResponse) {
+            //
+        }
+        
+        /**
+         * Update the specified resource in storage.
+         */
+        public function update(UpdateSurveyResponseRequest $request, Response $surveyResponse) {
+            //
+        }
+        
+        /**
+         * Remove the specified resource from storage.
+         */
+        public function destroy(Response $surveyResponse) {
+            //
+        }
+    }
--- app/Http/Requests/StoreSurveyResponseRequest.php
+++ app/Http/Requests/StoreSurveyResponseRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class IndexSurveyRespondentRequest extends FormRequest
+class StoreSurveyResponseRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
