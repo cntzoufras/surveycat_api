670ceb8a4686b342e2184092a1a5417bea6bd5d9
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-16 00:00:36 +0200

    Implement morph on comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,16 +2,19 @@
     
     namespace App\Models;
     
+    use App\Traits\Uuids;
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
     use Illuminate\Database\Eloquent\Relations\MorphTo;
     
     class Comment extends Model {
         
-        use HasFactory;
+        use HasFactory, Uuids;
         
-        protected $fillable = ['body', 'commentable_type'];
-        protected $guarded  = ['id'];
+        protected $fillable     = ['body', 'commentable_type', 'user_id'];
+        protected $guarded      = ['id'];
+        public    $incrementing = false;
+        protected $keyType      = 'string';
         
         public function commentable(): MorphTo {
             return $this->morphTo();
