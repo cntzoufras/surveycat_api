74bf6fbc184f56b1ac48ca778a8aac9f05bcd951
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-16 00:03:19 +0200

    Add morph relationship in tag

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,14 +2,33 @@
     
     namespace App\Models;
     
+    use App\Traits\Uuids;
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Relations\MorphToMany;
     
     class Tag extends Model {
         
-        use HasFactory;
+        use HasFactory, Uuids;
         
         protected $guarded = ['id'];
         
-        protected $fillable = [''];
+        /**
+         * Get all surveys that are assigned this tag.
+         *
+         * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
+         */
+        public function surveys(): MorphToMany {
+            return $this->morphedByMany(Survey::class, 'taggable');
+        }
+        
+        
+        /**
+         * Get all survey questions that are assigned this tag.
+         *
+         * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
+         */
+        public function survey_questions(): MorphToMany {
+            return $this->morphedByMany(SurveyQuestion::class, 'taggable');
+        }
     }
