ff97c8305b1c2c435eef84848d665ae49eb567ef
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-16 00:01:35 +0200

    Remove redundant relations stated in survey question

--- app/Models/LibraryQuestion.php
+++ app/Models/LibraryQuestion.php
@@ -5,9 +5,6 @@
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
     use Illuminate\Database\Eloquent\Relations\BelongsTo;
-    use Illuminate\Database\Eloquent\Relations\HasMany;
-    use Illuminate\Database\Eloquent\Relations\HasOne;
-    use Illuminate\Database\Eloquent\Relations\MorphMany;
     
     class LibraryQuestion extends Model {
         
@@ -17,24 +14,5 @@
         
         public function survey_question(): BelongsTo {
             return $this->belongsTo(SurveyQuestion::class);
-        }
-        
-        public function question_type(): hasOne {
-            return $this->hasOne(QuestionType::class);
-        }
-        
-        public function theme(): hasMany {
-            return $this->hasMany(Theme::class);
-        }
-        
-        public function tags() {
-            return $this->morphMany(Tag::class, 'taggable');
-        }
-        
-        /**
-         * Get all of the post's comments.
-         */
-        public function comments(): MorphMany {
-            return $this->morphMany(Comment::class, 'commentable');
         }
     }
