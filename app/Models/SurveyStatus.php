15dffa7e487d4da76d8d18965d9b8342a1c50e10
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-16 00:02:55 +0200

    Cleanup leftovers in survey status

--- app/Models/SurveyStatus.php
+++ app/Models/SurveyStatus.php
@@ -4,12 +4,8 @@
     
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Eloquent\Relations\BelongsTo;
     use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-    use Illuminate\Database\Eloquent\Relations\HasMany;
-    use Illuminate\Database\Eloquent\Relations\HasOne;
     use App\Traits\Uuids;
-    use Illuminate\Database\Eloquent\Relations\MorphMany;
     use Illuminate\Notifications\Notifiable;
     
     class SurveyStatus extends Model {
