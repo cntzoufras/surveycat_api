a5ff82f9dfb7bff8c405cd189b38a4319e99aa58
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-16 01:09:46 +0200

    Refactor migration times in all migrations

---
+++ database/migrations/2023_03_28_100215_create_survey_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+    
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
+    
+    return new class extends Migration {
+        
+        /**
+         * Run the migrations.
+         */
+        public function up(): void {
+            Schema::create('survey_settings', function (Blueprint $table) {
+                $table->id();
+                $table->string('page_title');
+                $table->boolean('show_page_title')->default(true);
+                $table->boolean('show_page_numbers')->default(true);
+                $table->boolean('show_question_numbers')->default(true);
+                $table->boolean('show_progress_bar')->default(false);
+                $table->boolean('required_asterisks')->default(true);
+                $table->string('public_link')->nullable()->default(null);
+                $table->string('banner_image')->nullable()->default(null);
+                $table->timestamps();
+                $table->foreignUuid('survey_id')->constrained('surveys');
+            });
+        }
+        
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void {
+            Schema::table('survey_settings', function (Blueprint $table) {
+                $table->dropForeign('survey_settings_survey_id_foreign');
+            });
+            Schema::dropIfExists('survey_settings');
+        }
+    };
