9332444c47d545a6125f9400ea39e97a46428e5e
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 01:47:06 +0200

    Store User submission data & use survey response

--- database/migrations/2023_03_28_100900_create_survey_submissions_table.php
+++ database/migrations/2023_03_28_100900_create_survey_submissions_table.php
@@ -1,32 +1,33 @@
 <?php
-    
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
-    
-    return new class extends Migration {
-        
-        /**
-         * Run the migrations.
-         */
-        public function up(): void {
-            Schema::create('survey_submissions', function (Blueprint $table) {
-                $table->uuid('id')->primary();
-                $table->timestamps();
-                $table->foreignUuId('survey_id')->references('id')->on('surveys')->onDelete('cascade');
-                $table->foreignUuId('survey_response_id')->references('id')->on('survey_responses');
-                $table->foreignUuid('respondent_id')->references('id')->on('respondents');
-            });
-        }
-        
-        /**
-         * Reverse the migrations.
-         */
-        public function down(): void {
-            Schema::table('survey_submissions', function (Blueprint $table) {
-                $table->dropForeign(['respondent_id']);
-                $table->dropForeign(['survey_response_id']);
-            });
-            Schema::dropIfExists('survey_submissions');
-        }
-    };
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('survey_submissions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->jsonb('submission_data')->default(null);
+            $table->foreignUuid('survey_id')->references('id')->on('surveys');
+            $table->foreignUuid('survey_response_id')->references('id')->on('survey_responses');
+            $table->foreignUuid('respondent_id')->references('id')->on('respondents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::table('survey_submissions', function (Blueprint $table) {
+            $table->dropForeign(['respondent_id']);
+            $table->dropForeign(['survey_response_id']);
+        });
+        Schema::dropIfExists('survey_submissions');
+    }
+};
