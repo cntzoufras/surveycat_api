27e8305341ecbd0712843d3cd626ef6e26c902aa
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 01:43:47 +0200

    Redundant created_by, code styling

--- database/migrations/2023_03_28_100323_create_themes_table.php
+++ database/migrations/2023_03_28_100323_create_themes_table.php
@@ -1,34 +1,32 @@
 <?php
-    
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
-    
-    return new class extends Migration {
-        
-        /**
-         * Run the migrations.
-         */
-        public function up(): void {
-            Schema::create('themes', function (Blueprint $table) {
-                $table->uuid('id')->primary();
-                $table->string('name');
-                $table->string('description');
-                $table->string('created_by');
-                $table->timestamps();
-                $table->foreignUuid('user_id')->constrained('users');
-                $table->foreignUuid('theme_setting_id')->constrained('theme_settings');
-            });
-        }
-        
-        /**
-         * Reverse the migrations.
-         */
-        public function down(): void {
-            Schema::table('themes', function (Blueprint $table) {
-                $table->dropForeign(['theme_setting_id']);
-                $table->dropForeign(['user_id']);
-            });
-            Schema::dropIfExists('themes');
-        }
-    };
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('themes', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('description');
+            $table->foreignUuid('user_id')->constrained('users');
+            $table->foreignUuid('theme_setting_id')->constrained('theme_settings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::table('themes', function (Blueprint $table) {
+            $table->dropForeign(['theme_setting_id', 'user_id']);
+        });
+        Schema::dropIfExists('themes');
+    }
+};
