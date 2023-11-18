6d57d4996f9271e69278c79a494f91379213e384
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 01:47:20 +0200

    Code styling

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,27 +1,28 @@
 <?php
-    
-    namespace App\Models;
-    
-    use App\Traits\Uuids;
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Eloquent\Relations\MorphTo;
-    use Illuminate\Database\Eloquent\SoftDeletes;
-    
-    class Comment extends Model {
-        
-        use HasFactory, Uuids, SoftDeletes;
-        
-        public    $incrementing = false;
-        protected $keyType      = 'string';
-        
-        protected $guarded  = ['id'];
-        protected $fillable = ['content', 'user_id'];
-        
-        /**
-         * @return \Illuminate\Database\Eloquent\Relations\MorphTo
-         */
-        public function commentable(): MorphTo {
-            return $this->morphTo();
-        }
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Comment extends Model {
+
+    use HasFactory, Uuids, SoftDeletes;
+
+    public    $incrementing = false;
+    protected $keyType      = 'string';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = ['content', 'user_id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function commentable(): MorphTo {
+        return $this->morphTo();
     }
+}
