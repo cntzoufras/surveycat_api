1cea115284135ed72039f0ae8e551a9a2974b8aa
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 01:48:49 +0200

    Add relations & docs

--- app/Models/QuestionType.php
+++ app/Models/QuestionType.php
@@ -1,26 +1,35 @@
 <?php
-    
-    namespace App\Models;
-    
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-    
-    class QuestionType extends Model {
-        
-        use HasFactory;
-        
-        public $timestamps = false;
-        
-        protected $guarded = ['id', 'title', 'description'];
-        
-        protected $fillable = [''];
-        
-        public function survey_questions(): belongsToMany {
-            return $this->belongsToMany(SurveyQuestion::class);
-        }
-        
-        public function library_question() {
-            return $this->belongsToMany(LibraryQuestion::class);
-        }
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class QuestionType extends Model {
+
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $guarded = ['id'];
+
+    /**
+     * Get all survey questions using this question type
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function survey_questions(): HasMany {
+        return $this->hasMany(SurveyQuestion::class);
     }
+
+    /**
+     * Get all library questions using this question type
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function library_question(): HasMany {
+        return $this->hasMany(LibraryQuestion::class);
+    }
+}
