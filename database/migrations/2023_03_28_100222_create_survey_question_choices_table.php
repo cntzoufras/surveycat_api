b0e43a408fffb33822cbf869d4e60cb7ea6b5a12
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 01:41:48 +0200

    Remove redundant question type, add sort index

--- database/migrations/2023_03_28_100222_create_survey_question_choices_table.php
+++ database/migrations/2023_03_28_100222_create_survey_question_choices_table.php
@@ -1,31 +1,31 @@
 <?php
-    
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
-    
-    return new class extends Migration {
-        
-        /**
-         * Run the migrations.
-         */
-        public function up(): void {
-            Schema::create('survey_question_choices', function (Blueprint $table) {
-                $table->id();
-                $table->string('title')->index();
-                $table->foreignId('question_type_id')->constrained('question_types');
-                $table->foreignUuid('survey_question_id')->constrained('survey_questions');
-                $table->timestamps();
-            });
-        }
-        
-        /**
-         * Reverse the migrations.
-         */
-        public function down(): void {
-            Schema::table('survey_question_choices', function (Blueprint $table) {
-                $table->dropForeign(['survey_question_id']);
-            });
-            Schema::dropIfExists('survey_question_choices');
-        }
-    };
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('survey_question_choices', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->index();
+            $table->integer('sort_index')->default(0);
+            $table->foreignUuid('survey_question_id')->constrained('survey_questions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::table('survey_question_choices', function (Blueprint $table) {
+            $table->dropForeign(['survey_question_id']);
+        });
+        Schema::dropIfExists('survey_question_choices');
+    }
+};
