5cad39513d4e0220ccaa114806ae70b486572a4e
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 01:59:32 +0200

    Remove user, refactor relations

--- app/Models/SurveyCategory.php
+++ app/Models/SurveyCategory.php
@@ -1,33 +1,27 @@
 <?php
-    
-    namespace App\Models;
-    
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Eloquent\Relations\BelongsTo;
-    use Illuminate\Database\Eloquent\Relations\MorphToMany;
-    
-    class SurveyCategory extends Model {
-        
-        use HasFactory;
-        
-        protected $guarded  = ['id'];
-        protected $fillable = ['title', 'description'];
-        
-        public function index() {
-        
-        }
-        
-        public function user(): BelongsTo {
-            return $this->belongsTo(User::class);
-        }
-        
-        public function survey(): BelongsTo {
-            return $this->belongsTo(Survey::class);
-        }
-        
-        public function tags(): MorphToMany {
-            return $this->morphToMany(Tag::class, 'taggable');
-        }
-        
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+class SurveyCategory extends Model {
+
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $fillable = ['title', 'description'];
+
+    public function surveys(): HasMany {
+        return $this->hasMany(Survey::class);
     }
+
+    public function tags(): MorphToMany {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
+
+}
