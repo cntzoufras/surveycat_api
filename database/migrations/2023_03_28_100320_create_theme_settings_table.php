d79427fd8797202fe307456a79bfa03400baf071
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 01:43:19 +0200

    Remove is_public, is_archived, code styling

--- database/migrations/2023_03_28_100320_create_theme_settings_table.php
+++ database/migrations/2023_03_28_100320_create_theme_settings_table.php
@@ -1,37 +1,34 @@
 <?php
-    
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
-    
-    return new class extends Migration {
-        
-        /**
-         * Run the migrations.
-         */
-        public function up(): void {
-            Schema::create('theme_settings', function (Blueprint $table) {
-                $table->uuid('id')->primary();
-                $table->string('title');
-                $table->string('footer');
-                $table->jsonb('settings'); // logo, fonts, primary_color, secondary_color
-                $table->text('image');
-                $table->boolean('is_public');
-                $table->boolean('is_archived');
-                $table->timestamps();
-                $table->foreignUuid('user_id')->constrained('users');
-                $table->foreignUuid('survey_id')->constrained('surveys');
-            });
-        }
-        
-        /**
-         * Reverse the migrations.
-         */
-        public function down(): void {
-            Schema::table('theme_settings', function (Blueprint $table) {
-                $table->dropForeign(['survey_id']);
-                $table->dropForeign(['user_id']);
-            });
-            Schema::dropIfExists('theme_settings');
-        }
-    };
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('theme_settings', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->string('footer');
+            $table->jsonb('settings'); // logo, fonts, primary_color, secondary_color
+            $table->text('image');
+            $table->foreignUuid('user_id')->constrained('users');
+            $table->foreignUuid('theme_id')->constrained('themes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::table('theme_settings', function (Blueprint $table) {
+            $table->dropForeign(['theme_id', 'user_id']);
+        });
+        Schema::dropIfExists('theme_settings');
+    }
+};
