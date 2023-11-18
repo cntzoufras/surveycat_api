b052f77a2c3c40cb57d7a83c0881ec830ad1c511
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 01:45:29 +0200

    Add response time logic fields

--- database/migrations/2023_03_28_100700_create_survey_responses_table.php
+++ database/migrations/2023_03_28_100700_create_survey_responses_table.php
@@ -1,32 +1,36 @@
 <?php
-    
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
-    
-    return new class extends Migration {
-        
-        /**
-         * Run the migrations.
-         */
-        public function up(): void {
-            Schema::create('survey_responses', function (Blueprint $table) {
-                $table->uuid('id')->primary();
-                $table->string('ip_address');
-                $table->string('device');
-                $table->unsignedBigInteger('completion_time')->nullable();
-                $table->timestamps();
-                $table->foreignUuid('survey_id')->constrained('surveys');
-            });
-        }
-        
-        /**
-         * Reverse the migrations.
-         */
-        public function down(): void {
-            Schema::table('survey_responses', function (Blueprint $table) {
-                $table->dropForeign(['survey_id']);
-            });
-            Schema::dropIfExists('survey_responses');
-        }
-    };
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('survey_responses', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('ip_address');
+            $table->string('device');
+            $table->timestamp('started_at')->useCurrent();
+            $table->timestamp('submitted_at')->default(null);
+            $table->string('session_id')->index();
+            $table->foreignUuid('survey_id')->constrained('surveys');
+            $table->foreignUuid('survey_submission_id')->constrained('survey_submissions');
+            $table->foreignUuid('respondent_id')->constrained('respondents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::table('survey_responses', function (Blueprint $table) {
+            $table->dropForeign(['survey_id', 'survey_submission_id', 'respondent_id']);
+        });
+        Schema::dropIfExists('survey_responses');
+    }
+};
