d64978daaf24f729668ed87bbc1c6f8bae37662d
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 01:40:22 +0200

    Add public link, & survey category, refactor column place

--- database/migrations/2023_03_28_100212_create_surveys_table.php
+++ database/migrations/2023_03_28_100212_create_surveys_table.php
@@ -1,36 +1,37 @@
 <?php
-    
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
-    
-    return new class extends Migration {
-        
-        /**
-         * Run the migrations.
-         */
-        public function up(): void {
-            Schema::create('surveys', function (Blueprint $table) {
-                $table->uuid('id')->primary();
-                $table->string('title');
-                $table->string('description')->nullable();
-                $table->timestamps();
-                $table->integer('views')->default(0);
-                $table->softDeletes();
-                $table->foreignId('survey_category_id')->constrained('survey_categories');
-                $table->foreignId('survey_status_id')->constrained('survey_statuses');
-                $table->index(['id', 'title']);
-            });
-        }
-        
-        /**
-         * Reverse the migrations.
-         */
-        public function down(): void {
-            Schema::table('surveys', function (Blueprint $table) {
-                $table->dropForeign(['survey_category_id']);
-                $table->dropForeign(['survey_status_id']);
-            });
-            Schema::dropIfExists('surveys');
-        }
-    };
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title')->index();
+            $table->string('description')->nullable();
+            $table->string('public_link')->nullable()->default(null);
+            $table->integer('views')->default(0);
+            $table->foreignId('survey_category_id')->constrained('survey_categories');
+            $table->foreignId('theme_id')->constrained('themes');
+            $table->foreignId('survey_status_id')->constrained('survey_statuses');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::table('surveys', function (Blueprint $table) {
+            $table->dropForeign(['survey_category_id']);
+            $table->dropForeign(['survey_status_id']);
+        });
+        Schema::dropIfExists('surveys');
+    }
+};
