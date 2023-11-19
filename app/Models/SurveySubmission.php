a9ec29b095166c413edf7e756b9cef3fe3d973b1
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 02:20:42 +0200

    Refactor deprecated model, add relations, add docs

--- app/Models/SurveySubmission.php
+++ app/Models/SurveySubmission.php
@@ -1,29 +1,48 @@
 <?php
-    
-    namespace App\Models;
-    
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Eloquent\Relations\BelongsTo;
-    use Illuminate\Database\Eloquent\Relations\HasMany;
-    
-    class SurveySubmission extends Model {
-        
-        use HasFactory;
-        
-        protected $guarded  = ['uuid'];
-        protected $fillable = ['pages', 'uv', 'pv', 'amt', 'name', 'description', 'style_options'];
-        
-        
-        public function survey(): BelongsTo {
-            return $this->belongsTo(Survey::class);
-        }
-        
-        public function respondent(): BelongsTo {
-            return $this->belongsTo(Respondent::class);
-        }
-        
-        public function survey_questions(): HasMany {
-            return $this->hasMany(SurveyQuestion::class);
-        }
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SurveySubmission extends Model {
+
+    use HasFactory, Uuids;
+
+    public    $incrementing = false;
+    protected $keyType      = 'string';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = ['survey_id', 'survey_response_id', 'respondent_id'];
+
+    /**
+     * Get the survey of this submission
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function survey(): BelongsTo {
+        return $this->belongsTo(Survey::class);
     }
+
+    /**
+     * Get the response associated with this submission
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function survey_response(): BelongsTo {
+        return $this->belongsTo(SurveyResponse::class);
+    }
+
+    /**
+     * Get the respondent of the submission
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function respondent(): BelongsTo {
+        return $this->belongsTo(Respondent::class);
+    }
+
+}
