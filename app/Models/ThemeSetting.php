17e82f3da2b0d64020b99167bad343696443870c
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 02:28:53 +0200

    Refactor model, add relations, add docs

--- app/Models/ThemeSetting.php
+++ app/Models/ThemeSetting.php
@@ -1,26 +1,40 @@
 <?php
-    
-    namespace App\Models;
-    
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Eloquent\Relations\BelongsTo;
-    use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-    
-    class ThemeSetting extends Model {
-        
-        use HasFactory;
-        
-        protected $fillable = ['name', 'description',];
-        
-        protected $guarded = ['id'];
-        
-        public function theme(): BelongsToMany {
-            return $this->belongsTomany(Theme::class);
-        }
-        
-        public function theme_variable() {
-            return $this->belongsTo(ThemeVariable::class);
-        }
-        
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class ThemeSetting extends Model {
+
+    use HasFactory, Uuids;
+
+    public    $incrementing = false;
+    protected $keyType      = 'string';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = ['title', 'footer', 'settings', 'image'];
+
+    /**
+     * Get the themes
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function theme(): BelongsToMany {
+        return $this->belongsTomany(Theme::class, 'theme_id', 'id');
     }
+
+    /**
+     * Get the theme variable for this theme setting
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function theme_variable(): BelongsTo {
+        return $this->belongsTo(ThemeVariable::class, 'theme_variable_id', 'id');
+    }
+
+}
