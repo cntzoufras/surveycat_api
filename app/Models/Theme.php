d20d6c28c017106828d59db97acd07a73fbf03a4
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 02:26:42 +0200

    Refactor model, add relations, add docs

--- app/Models/Theme.php
+++ app/Models/Theme.php
@@ -1,20 +1,39 @@
 <?php
-    
-    namespace App\Models;
-    
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Eloquent\Relations\BelongsTo;
-    
-    class Theme extends Model {
-        
-        use HasFactory;
-        
-        protected $guarded = ['id'];
-        
-        protected $fillable = [''];
-        
-        public function theme_setting(): BelongsTo {
-            return $this->belongsTo(ThemeSetting::class);
-        }
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Theme extends Model {
+
+    use HasFactory, Uuids;
+
+    public    $incrementing = false;
+    protected $keyType      = 'string';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = ['name', 'description', 'user_id', 'theme_setting_id'];
+
+    /**
+     * Get the theme settings
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function theme_setting(): BelongsTo {
+        return $this->belongsTo(ThemeSetting::class, 'theme_setting_id', 'id');
     }
+
+    /**
+     * Get the surveys using this theme
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function survey(): BelongsToMany {
+        return $this->belongsToMany(Survey::class);
+    }
+}
