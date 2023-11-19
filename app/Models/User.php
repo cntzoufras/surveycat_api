b823e14c8b74847a4359a67ca3de18b5ebee30d7
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-19 02:29:54 +0200

    Code styling & remove submissions from users

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,52 +1,48 @@
 <?php
-    
-    namespace App\Models;
-    
-    use Illuminate\Contracts\Auth\MustVerifyEmail;
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Relations\HasMany;
-    use Illuminate\Foundation\Auth\User as Authenticatable;
-    use Illuminate\Notifications\Notifiable;
-    use App\Traits\Uuids;
-    
-    class User extends Authenticatable {
-        
-        use HasFactory, Notifiable, Uuids;
-        
-        protected $guarded = ['uuid'];
-        
-        
-        /**
-         * The attributes that are mass assignable.
-         *
-         * @var array
-         */
-        protected $fillable = [
-            'username',
-            'email',
-            'password',
-        ];
-        
-        /**
-         * The attributes that should be hidden for arrays.
-         *
-         * @var array
-         */
-        protected $hidden = [
-            'password',
-            'remember_token',
-        ];
-        
-        /**
-         * The attributes that should be cast to native types.
-         *
-         * @var array
-         */
-        protected $casts = [
-            'email_verified_at' => 'datetime',
-        ];
-        
-        public function survey_submissions(): HasMany {
-            return $this->hasMany(SurveySubmission::class);
-        }
-    }
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use App\Traits\Uuids;
+
+class User extends Authenticatable {
+
+    use HasFactory, Notifiable, Uuids;
+
+    public    $incrementing = false;
+    protected $keyType      = 'string';
+
+    protected $guarded = ['id'];
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['username', 'email', 'password', 'first_name', 'middle_name', 'last_name'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+}
