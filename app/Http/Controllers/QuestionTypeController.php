3bd648085de4ea7f065f0d97cd5a5f80c18028d3
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-21 00:23:45 +0200

    Refactor QuestionType controller

--- app/Http/Controllers/QuestionTypeController.php
+++ app/Http/Controllers/QuestionTypeController.php
@@ -1,59 +1,59 @@
 <?php
-    
-    namespace App\Http\Controllers;
-    
-    use App\Http\Requests\StoreQuestionTypeRequest;
-    use App\Http\Requests\UpdateQuestionTypeRequest;
-    use App\Models\SurveyQuestionType;
-    
-    class QuestionTypeController extends Controller {
-        
-        /**
-         * Display a listing of the resource.
-         */
-        public function index() {
-            //
-        }
-        
-        /**
-         * Show the form for creating a new resource.
-         */
-        public function create() {
-            //
-        }
-        
-        /**
-         * Store a newly created resource in storage.
-         */
-        public function store(StoreQuestionTypeRequest $request) {
-            //
-        }
-        
-        /**
-         * Display the specified resource.
-         */
-        public function show(SurveyQuestionType $questionType) {
-            //
-        }
-        
-        /**
-         * Show the form for editing the specified resource.
-         */
-        public function edit(SurveyQuestionType $questionType) {
-            //
-        }
-        
-        /**
-         * Update the specified resource in storage.
-         */
-        public function update(UpdateQuestionTypeRequest $request, SurveyQuestionType $questionType) {
-            //
-        }
-        
-        /**
-         * Remove the specified resource from storage.
-         */
-        public function destroy(SurveyQuestionType $questionType) {
-            //
-        }
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\QuestionType\StoreQuestionTypeRequest;
+use App\Http\Requests\QuestionType\UpdateQuestionTypeRequest;
+use App\Models\QuestionType;
+
+class QuestionTypeController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() {
+        //
     }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreQuestionTypeRequest $request) {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(QuestionType $questionType) {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(QuestionType $questionType) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateQuestionTypeRequest $request, QuestionType $questionType) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(QuestionType $questionType) {
+        //
+    }
+}
--- database/factories/SurveyFactory.php
+++ database/factories/SurveyFactory.php
@@ -1,34 +1,34 @@
 <?php
-    
-    namespace Database\Factories;
-    
-    use Illuminate\Database\Eloquent\Factories\Factory;
-    
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Survey\Survey>
+ */
+class SurveyFactory extends Factory {
+
     /**
-     * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Survey>
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
      */
-    class SurveyFactory extends Factory {
-        
-        /**
-         * Define the model's default state.
-         *
-         * @return array<string, mixed>
-         */
-        public function definition(): array {
-            return [
-                'title'                 => fake()->sentence(),
-                'page_title'            => fake()->sentence(),
-                'show_page_title'       => fake()->numberBetween(0, 1),
-                'show_page_numbers'     => fake()->numberBetween(0, 1),
-                'show_question_numbers' => fake()->numberBetween(0, 1),
-                'show_progress_bar'     => fake()->numberBetween(0, 1),
-                'required_asterisks'    => fake()->numberBetween(0, 1),
-                'public_link'           => fake()->url(),
-                'banner_image'          => fake()->imageUrl(),
-                'views'                 => fake()->numberBetween(0, 1000000),
-                'survey_category_id'    => fake()->numberBetween(1, 12),
-                'survey_status_id'      => fake()->numberBetween(1, 5),
-                'theme_setting_id'      => fake()->numberBetween(1, 5),
-            ];
-        }
+    public function definition(): array {
+        return [
+            'title'                 => fake()->sentence(),
+            'page_title'            => fake()->sentence(),
+            'show_page_title'       => fake()->numberBetween(0, 1),
+            'show_page_numbers'     => fake()->numberBetween(0, 1),
+            'show_question_numbers' => fake()->numberBetween(0, 1),
+            'show_progress_bar'     => fake()->numberBetween(0, 1),
+            'required_asterisks'    => fake()->numberBetween(0, 1),
+            'public_link'           => fake()->url(),
+            'banner_image'          => fake()->imageUrl(),
+            'views'                 => fake()->numberBetween(0, 1000000),
+            'survey_category_id'    => fake()->numberBetween(1, 12),
+            'survey_status_id'      => fake()->numberBetween(1, 5),
+            'theme_setting_id'      => fake()->numberBetween(1, 5),
+        ];
     }
+}
