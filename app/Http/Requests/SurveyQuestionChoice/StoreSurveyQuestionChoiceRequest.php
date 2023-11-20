99fe916b7561e6361ee43145234ded3a9eeb2c2b
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-21 00:16:46 +0200

    Add store/update requests

--- app/Http/Requests/SurveyQuestionChoice/StoreSurveyQuestionChoiceRequest.php
+++ app/Http/Requests/SurveyQuestionChoice/StoreSurveyQuestionChoiceRequest.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\SurveyQuestionChoice;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateSurveySettingsRequest extends FormRequest
-{
+class StoreSurveyQuestionChoiceRequest extends FormRequest {
+
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
+    public function authorize(): bool {
         return false;
     }
 
@@ -19,8 +18,7 @@ class UpdateSurveySettingsRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
             //
         ];
