c04b60ddc7ee033c643255d2f31bbcc852414a1d
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-21 00:36:10 +0200

    Remove s from name

--- app/Http/Requests/VariablePalette/UpdateVariablePaletteRequest.php
+++ app/Http/Requests/VariablePalette/UpdateVariablePaletteRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\VariablePalette;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateVariablePalettesRequest extends FormRequest {
+class UpdateVariablePaletteRequest extends FormRequest {
 
     /**
      * Determine if the user is authorized to make this request.
