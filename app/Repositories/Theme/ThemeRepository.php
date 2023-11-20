f4a6b8c66c3e114246358e028ed0e03c902b4a9d
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-21 00:00:27 +0200

    Add Repositories

---
+++ app/Repositories/Theme/ThemeRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repositories\Survey\Theme;
+
+use App\Models\Theme\Theme;
+use Illuminate\Support\Facades\DB;
+
+class ThemeRepository {
+
+    public function index(array $params) {
+
+        try {
+            $limit = $params['limit'] ?? 10;
+            return DB::transaction(function () use ($limit) {
+                return Theme::query()->paginate($limit);
+            });
+        } catch (\Exception $e) {
+            throw new \Exception($e, 500);
+        }
+    }
+
+    public function resolveModel($theme) {
+        if ($theme instanceof Theme) {
+            return $theme;
+        }
+        return Theme::query()->findOrFail($theme);
+    }
+
+    public function getIfExist($theme) {
+        return Theme::query()->find($theme);
+    }
+
+    public function update(Theme $theme, array $params) {
+        return DB::transaction(function () use ($params, $theme) {
+            $theme->fill($params);
+            $theme->save();
+            return $theme;
+        });
+    }
+
+    public function store(array $params): Theme {
+        return DB::transaction(function () use ($params) {
+            $theme = new Theme();
+            $theme->fill($params);
+            $theme->save();
+            return $theme;
+        });
+    }
+
+    public function delete(Theme $theme) {
+        return DB::transaction(function () use ($theme) {
+            $theme->delete();
+            return $theme;
+        });
+    }
+
+}
---
+++ app/Repositories/Theme/VariablePaletteRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repositories\Survey\Theme;
+
+use App\Models\Theme\VariablePalette;
+use Illuminate\Support\Facades\DB;
+
+class VariablePaletteRepository {
+
+    public function index(array $params) {
+
+        try {
+            $limit = $params['limit'] ?? 10;
+            return DB::transaction(function () use ($limit) {
+                return VariablePalette::query()->paginate($limit);
+            });
+        } catch (\Exception $e) {
+            throw new \Exception($e, 500);
+        }
+    }
+
+    public function resolveModel($variable_palette) {
+        if ($variable_palette instanceof VariablePalette) {
+            return $variable_palette;
+        }
+        return VariablePalette::query()->findOrFail($variable_palette);
+    }
+
+    public function getIfExist($variable_palette) {
+        return VariablePalette::query()->find($variable_palette);
+    }
+
+    public function update(VariablePalette $variable_palette, array $params) {
+        return DB::transaction(function () use ($params, $variable_palette) {
+            $variable_palette->fill($params);
+            $variable_palette->save();
+            return $variable_palette;
+        });
+    }
+
+    public function store(array $params): VariablePalette {
+        return DB::transaction(function () use ($params) {
+            $variable_palette = new VariablePalette();
+            $variable_palette->fill($params);
+            $variable_palette->save();
+            return $variable_palette;
+        });
+    }
+
+    public function delete(VariablePalette $variable_palette) {
+        return DB::transaction(function () use ($variable_palette) {
+            $variable_palette->delete();
+            return $variable_palette;
+        });
+    }
+
+}
