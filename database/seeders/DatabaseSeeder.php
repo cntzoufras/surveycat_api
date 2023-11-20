805bdde3c23720b7b57a540a88da5bac0c1d03d2
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-11-21 00:13:06 +0200

    Update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,26 +1,23 @@
 <?php
-    
-    namespace Database\Seeders;
-    
-    use App\Models\SurveyCategory;
-    use App\Models\User;
-    use Illuminate\Database\Seeder;
-    use UserSeeder;
-    
-    class DatabaseSeeder extends Seeder {
-        
-        /**
-         * Seed the application's database.
-         *
-         * @return void
-         */
-        public function run() {
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder {
+
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run() {
 //            \App\Models\Question::factory(2000)->create();
-//            \App\Models\SurveyPage::factory(5)->create();
+//            \App\Models\Survey\SurveyPage::factory(5)->create();
 //            $this->call(UserSeeder::class);
-            \App\Models\User::factory(50)->create();
-            $this->call(QuestionTypeSeeder::class);
-            $this->call(SurveyCategorySeeder::class);
-            $this->call(SurveyStatusSeeder::class);
-        }
+        \App\Models\User::factory(50)->create();
+        $this->call(QuestionTypeSeeder::class);
+        $this->call(SurveyCategorySeeder::class);
+        $this->call(SurveyStatusSeeder::class);
     }
+}
