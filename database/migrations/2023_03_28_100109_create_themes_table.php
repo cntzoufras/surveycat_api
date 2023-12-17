d53c8f381e59af2397284f27e99f213903c36f10
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-12-17 15:18:11 +0200

    Add Themes

---
+++ database/migrations/2023_03_28_100109_create_themes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('themes', function (Blueprint $table) {
+            if (DB::connection()->getDriverName() === 'pgsql') {
+                DB::statement('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+            }
+            $table->uuid('id')->primary()->default(DB::raw('uuid_generate_v4()'));
+            $table->string('title')->index();
+            $table->string('description')->nullable();
+            $table->longText('footer')->nullable();
+            $table->foreignUuid('user_id')->nullable()->constrained('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::table('themes', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::dropIfExists('themes');
+    }
+};
