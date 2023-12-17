d7cd4c767cb7faefab35d520ac2e6f273ab95124
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-12-17 17:23:12 +0200

    Fix errors in Policy

--- app/Policies/SurveySettingsPolicy.php
+++ app/Policies/SurveySettingsPolicy.php
@@ -12,48 +12,48 @@ class SurveySettingsPolicy {
      */
     public function viewAny(User $user): bool {
         //
-//        return;
+        return false;
     }
 
     /**
      * Determine whether the user can view the model.
      */
     public function view(User $user, SurveySettings $surveySettings): bool {
-        //
+        return false;
     }
 
     /**
      * Determine whether the user can create models.
      */
     public function create(User $user): bool {
-        //
+        return false;
     }
 
     /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, SurveySettings $surveySettings): bool {
-        //
+        return false;
     }
 
     /**
      * Determine whether the user can delete the model.
      */
     public function delete(User $user, SurveySettings $surveySettings): bool {
-        //
+        return false;
     }
 
     /**
      * Determine whether the user can restore the model.
      */
     public function restore(User $user, SurveySettings $surveySettings): bool {
-        //
+        return false;
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
     public function forceDelete(User $user, SurveySettings $surveySettings): bool {
-        //
+        return false;
     }
 }
