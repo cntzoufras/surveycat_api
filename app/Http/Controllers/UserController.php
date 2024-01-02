3c3a5a24e5fd11d95d1a9934e01890c595a81b31
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-01-02 20:44:16 +0200

    Deploy new version

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SurveyQuestion\StoreSurveyQuestionRequest;
+use App\Http\Requests\SurveyQuestion\UpdateSurveyQuestionRequest;
+use App\Models\Survey\SurveyQuestion;
+use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
-class UserController extends Controller
-{
-    //
+class UserController extends Controller {
+
+    protected UserService $user_service;
+    private               $user_id = User::class('getCurrentUserId');
+
+    public function __construct(UserService $user_service) {
+        $this->user_service = $user_service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @throws \Exception
+     */
+    public function index(Request $request) {
+        $validated = $request->validate(['limit' => 'integer|sometimes|min:0|max:100']);
+        return $this->user_service->getUserRegisteredUsers($user_id);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSurveyQuestionRequest $request): SurveyQuestion {
+        return $this->user_service->store($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @throws \Exception
+     */
+    public function show(Request $request): mixed {
+        try {
+            if (isset($request['id'])) {
+                Validator::validate(['id' => $request['id']], [
+                    'id' => 'uuid|required|exists:survey_questions,id',
+                ]);
+                return $this->user_service->show($request['id']);
+            }
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage(), 500);
+        }
+        return null;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSurveyQuestionRequest $request, SurveyQuestion $survey_question) {
+        return $this->user_service->update($survey_question, $request->validated());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SurveyQuestion $survey_question) {
+        return $this->user_service->delete($survey_question);
+    }
 }
