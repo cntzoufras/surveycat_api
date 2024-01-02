8da30c9f6e79325055a00c922b51d69821888a19
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-01-02 04:11:36 +0200

    Use entrypoint for echoing to hosts

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Add entries to /etc/hosts
+echo "172.19.0.5 surveycat_api_surveycat-api_1" >> /etc/hosts
+echo "surveycat_api_surveycat-api_1 surveycat_api_surveycat-api_1" >> /etc/hosts
+
+# Start PHP-FPM
+php-fpm
