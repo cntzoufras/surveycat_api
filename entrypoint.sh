7360d5106e3b9bf1acfb965e83bb754ffc2c6fbb
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-01-02 15:04:00 +0200

    Migrate completely to Apache

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,3 @@
 # Add entries to /etc/hosts
 echo "172.19.0.5 surveycat_api_surveycat-api_1" >> /etc/hosts
 echo "surveycat_api_surveycat-api_1 surveycat_api_surveycat-api_1" >> /etc/hosts
-
-# Start PHP-FPM
-php-fpm
