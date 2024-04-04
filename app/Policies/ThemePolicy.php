e31726f018a19025a1d25add3ee6810d248d3832
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-04-04 18:54:31 +0300

    Return true in Theme policy

--- app/Policies/ThemePolicy.php
+++ app/Policies/ThemePolicy.php
@@ -11,28 +11,28 @@ class ThemePolicy {
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool {
-        //
+        return true;
     }
 
     /**
      * Determine whether the user can view the model.
      */
     public function view(User $user, Theme $theme): bool {
-        //
+        return true;
     }
 
     /**
      * Determine whether the user can create models.
      */
     public function create(User $user): bool {
-        //
+        return true;
     }
 
     /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Theme $theme): bool {
-        //
+        return true;
     }
 
     /**
