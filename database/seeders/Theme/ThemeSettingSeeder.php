7cc6190d5e0ab306648fc8d1ee08c0f5687eb531
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-04-04 18:55:47 +0300

    Create ThemeSettingSeeder

--- database/seeders/Theme/ThemeSettingSeeder.php
+++ database/seeders/Theme/ThemeSettingSeeder.php
@@ -12,24 +12,41 @@ class ThemeSettingSeeder extends Seeder {
      * Run the database seeds.
      */
     public function run(): void {
-        $csvPath = base_path('database/seeds/themes_settings.csv');
+        $csvPath = base_path('database/seeds/theme_settings.csv');
+        if (!file_exists($csvPath) || !is_readable($csvPath)) {
+            throw new \RuntimeException("Unable to find or read the file at: {$csvPath}");
+        }
+
+        $themes = Theme::query()->pluck('id')->all(); // Fetch all theme IDs
+        $theme_index = 0;
+        $theme_count = count($themes);
+
         $file = fopen($csvPath, 'r');
+
         fgetcsv($file);
         while (($data = fgetcsv($file)) !== false) {
-            $theme_setting = [$data[0]];
-            $this->seedThemeSetting($theme_setting);
+            if (!empty($data[0])) {
+                $theme_id = $themes[$theme_index % $theme_count];
+                $this->seedThemeSetting($data[0], $theme_id);
+                $theme_index++;
+            }
         }
         fclose($file);
     }
 
-    private function seedThemeSetting(array $theme_setting): void {
-        $theme_id = $this->getThemeId();
-        ThemeSetting::query()->create(['settings' => json_decode($theme_setting[0], true),
-                                       'theme_id' => $theme_id,
+    private function seedThemeSetting(string $theme_setting, $theme_id): void {
+
+        $decoded = json_decode($theme_setting, true);
+
+        if (!is_array($decoded)) {
+            \Log::warning("JSON decoding failed or didn't produce an array", ['json' => $theme_setting]);
+            return;
+        }
+
+        ThemeSetting::query()->create([
+            'settings' => $decoded,
+            'theme_id' => $theme_id,
         ]);
     }
 
-    private function getThemeId() {
-        return Theme::query()->inRandomOrder()->create()->first()->id;
-    }
 }
