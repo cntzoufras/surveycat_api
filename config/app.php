4aa1db16a3df61a28797dfa23519d74659372df4
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-04-04 19:26:27 +0300

    Update app for Sanctum

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         App\Providers\FieldsValidationServiceProvider::class,
+        Laravel\Sanctum\SanctumServiceProvider::class,
 
         /*
          * Package Service Providers...
