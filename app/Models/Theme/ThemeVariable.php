bcfba8a1cc31a49c618ba27b0fd4417f175686f8
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-04-04 18:56:42 +0300

    Update ThemeVariable model

--- app/Models/Theme/ThemeVariable.php
+++ app/Models/Theme/ThemeVariable.php
@@ -14,14 +14,14 @@ class ThemeVariable extends Model {
     protected $keyType      = 'string';
 
     protected $guarded  = ['id'];
-    protected $fillable = ['primary_background_alpha', 'theme_thumb', 'theme_id'];
+    protected $fillable = ['primary_background_alpha', 'theme_thumb', 'theme_setting_id'];
 
     /**
      * Get the theme associated with this variable.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function theme(): BelongsTo {
-        return $this->belongsTo(Theme::class, 'theme_id', 'id');
+    public function theme_setting(): BelongsTo {
+        return $this->belongsTo(ThemeSetting::class, 'theme_setting_id', 'id');
     }
 }
