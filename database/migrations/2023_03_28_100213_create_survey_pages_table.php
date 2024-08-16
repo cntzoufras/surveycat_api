c302b748056b5a69a3f2d99fb3addecf12570c42
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-16 20:34:27 +0300

    Add nullable title and sort index from front

--- database/migrations/2023_03_28_100213_create_survey_pages_table.php
+++ database/migrations/2023_03_28_100213_create_survey_pages_table.php
@@ -16,10 +16,10 @@ return new class extends Migration {
                 DB::statement('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
             }
             $table->uuid('id')->primary()->default(DB::raw('uuid_generate_v4()'));
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->string('description')->nullable();
             $table->enum('layout', ['single', 'multiple'])->default('multiple');
-            $table->unsignedInteger('sort_index')->default(0)->index();
+            $table->unsignedInteger('sort_index')->nullable()->default(0)->index();
             $table->boolean('require_questions')->default(false);
             $table->softDeletes();
             $table->timestamps();
