55b5d2b79e8d2293d6e3eaf19498b22697154fc3
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-16 20:35:02 +0300

    Update Morph Relations

--- database/migrations/2023_03_28_100214_create_tags_table.php
+++ database/migrations/2023_03_28_100214_create_tags_table.php
@@ -1,30 +1,31 @@
 <?php
-    
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
-    
-    return new class extends Migration {
-        
-        /**
-         * Run the migrations.
-         */
-        public function up(): void {
-            Schema::create('tags', function (Blueprint $table) {
-                $table->id();
-                $table->string('content')->index();
-                $table->timestamps();
-                $table->foreignUuid('user_id')->constrained('users');
-            });
-        }
-        
-        /**
-         * Reverse the migrations.
-         */
-        public function down(): void {
-            Schema::table('tags', function (Blueprint $table) {
-                $table->dropForeign(['user_id']);
-            });
-            Schema::dropIfExists('tags');
-        }
-    };
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('content')->index();
+            $table->timestamps();
+            $table->foreignUuid('user_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::dropIfExists('tags');
+    }
+};
