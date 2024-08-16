09e38757861ed100d1cf3b903817edc9da427a78
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-16 22:12:27 +0300

    Add newly registered User & Password reset Events in EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,31 +2,42 @@
 
 namespace App\Providers;
 
+use App\Events\UserRegistered;
+use App\Listeners\SendWelcomeEmailListener;
+use App\Events\PasswordResetRequested;
+use App\Listeners\SendPasswordResetNotification;
+
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
-class EventServiceProvider extends ServiceProvider
-{
+class EventServiceProvider extends ServiceProvider {
+
     /**
      * The event listener mappings for the application.
      *
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        Registered::class             => [
+            SendEmailVerificationNotification::class, // Laravel's default verification email
+        ],
+        UserRegistered::class         => [
+            SendWelcomeEmailListener::class,
+        ],
+        PasswordResetRequested::class => [
+            SendPasswordResetNotification::class,
         ],
     ];
+
 
     /**
      * Register any events for your application.
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         //
     }
 }
