f8a89eea01c383551cde22f8f284566020ff32c2
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-17 14:42:57 +0300

    Finalize UpdateSurveyPageRequest after front

---
+++ app/Http/Requests/Survey/UpdateSurveyRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Survey;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSurveyRequest extends FormRequest {
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array {
+        return [
+            'title'            => 'sometimes|string|max:255',
+            'description'      => 'sometimes|string|max:255',
+            'survey_status_id' => 'sometimes|integer|in:1,2,3,4',
+            'theme_id'         => 'sometimes|uuid|exists:themes,id',
+            'priority'         => 'sometimes|in:low,medium,high',
+            'user_id'          => 'sometimes|uuid|exists:users,id',
+        ];
+    }
+}
