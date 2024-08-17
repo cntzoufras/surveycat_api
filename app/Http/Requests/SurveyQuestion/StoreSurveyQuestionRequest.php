ea1fee0a94ffea945c390a2f085e8336026584b5
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-17 14:04:57 +0300

    Remove Tags from StoreSurveyQuestionRequest

--- app/Http/Requests/SurveyQuestion/StoreSurveyQuestionRequest.php
+++ app/Http/Requests/SurveyQuestion/StoreSurveyQuestionRequest.php
@@ -20,16 +20,10 @@ class StoreSurveyQuestionRequest extends BaseRequest {
      */
     public function rules(): array {
         return [
-            'title'                           => 'required|string|max:255',
-            'is_required'                     => 'required|boolean',
-            'question_type_id'                => 'required|integer|exists:question_types,id',
-            'survey_page_id'                  => 'required|uuid|exists:survey_pages,id',
-            'additional_settings'             => 'sometimes|array|allowed_keys:color,align,font_style,font_size,font_family',
-            'additional_settings.color'       => ['required', 'regex:/^#(?:[0-9a-fA-F]{3}){1,2}$/'],
-            'additional_settings.align'       => 'in:left,center,right',
-            'additional_settings.font_style'  => 'in:bold,italic,underline',
-            'additional_settings.font_family' => 'in:Arial,Calibri,Verdana',
-            'additional_settings.font_size'   => 'integer|gte:4|lte:40',
+            'title'            => 'required|string|max:255',
+            'is_required'      => 'required|boolean',
+            'question_type_id' => 'required|integer|exists:question_types,id',
+            'survey_page_id'   => 'required|uuid|exists:survey_pages,id',
         ];
     }
 }
