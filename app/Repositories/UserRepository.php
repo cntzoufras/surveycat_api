8ad63a85e8ae2141f8971478b0f97367244ff75b
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-17 12:14:11 +0300

    Update User Repository add find by reset token

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -1,22 +1,33 @@
 <?php
-    
-    namespace App\Repositories;
-    
-    use App\Models\User;
-    
-    class UserRepository {
-        
-        public function findByEmail($email): User {
-            /** @var User $user */
-            $user = User::query()
-                        ->where('email', 'ILIKE', $email)
-                        ->firstOrFail();
-            return $user;
-        }
-        
-        
-        public function findById($id) {
-            $user = User::query()->where('id', $id)->first();
-            return $user;
-        }
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository {
+
+    public function findByUsername(string $username): ?User {
+        return User::query()->where('username', $username)->first();
     }
+
+    public function findByEmail($email): User {
+        /** @var User $user */
+        $user = User::query()
+                    ->where('email', $email)
+                    ->firstOrFail();
+        return $user;
+    }
+
+    public function findById($id) {
+        return User::query()->where('id', $id)->first();
+    }
+
+    public function findByResetToken($token) {
+        return User::where('password_reset_token', $token)->first();
+    }
+
+    public function save(User $user): void {
+        $user->save();
+    }
+
+}
