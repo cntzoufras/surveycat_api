7796840f1fb7215cd5b27d748e2680c872d0622e
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-17 14:37:38 +0300

    Update basic methods in UserRepository & Controller from front

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\SurveyQuestion\StoreSurveyQuestionRequest;
-use App\Http\Requests\SurveyQuestion\UpdateSurveyQuestionRequest;
-use App\Models\Survey\SurveyQuestion;
 use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
+use UpdateUserRequest;
 
 class UserController extends Controller {
 
@@ -18,13 +15,6 @@ class UserController extends Controller {
 
     public function __construct(UserService $user_service) {
         $this->user_service = $user_service;
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreUserRequest $request): User {
-        return $this->user_service->store($request->validated);
     }
 
     /**
@@ -53,12 +43,5 @@ class UserController extends Controller {
      */
     public function update(UpdateUserRequest $request, $id) {
         return $this->user_service->update($id, $request->validated());
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user) {
-        return $this->user_service->destroy($user);
     }
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\Repositories;
 
+use App\Models\Survey\Survey;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserRepository {
 
@@ -24,6 +26,25 @@ class UserRepository {
 
     public function findByResetToken($token) {
         return User::where('password_reset_token', $token)->first();
+    }
+
+    public function getIfExist($user) {
+        return User::query()->find($user);
+    }
+
+    public function resolveModel($users) {
+        if ($users instanceof User) {
+            return $users;
+        }
+        return User::query()->findOrFail($users);
+    }
+
+    public function update(User $user, array $params) {
+        return DB::transaction(function () use ($params, $user) {
+            $user->fill($params);
+            $user->save();
+            return $user;
+        });
     }
 
     public function save(User $user): void {
