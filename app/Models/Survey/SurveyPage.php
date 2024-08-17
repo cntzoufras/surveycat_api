67514017530017633c2703f0eefb3d27b5079315
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-17 14:24:14 +0300

    Update SurveyModel to use Uuids and rename align to layout

--- app/Models/Survey/SurveyPage.php
+++ app/Models/Survey/SurveyPage.php
@@ -2,20 +2,22 @@
 
 namespace App\Models\Survey;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class SurveyPage extends Model {
 
-    use HasFactory, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes, Uuids;
 
     public    $incrementing = false;
     protected $keyType      = 'string';
 
     protected $guarded  = ['id'];
-    protected $fillable = ['title', 'description', 'align', 'sort_index', 'require_questions', 'survey_id'];
+    protected $fillable = ['title', 'description', 'layout', 'sort_index', 'require_questions', 'survey_id'];
 
     /**
      * Get the survey questions associated with the survey page.
