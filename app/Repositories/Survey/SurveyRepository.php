b97d478122a5d682c46332eecae5685a29c2fe16
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-17 14:53:49 +0300

    Add getStockSurveys in SurveyRepository

--- app/Repositories/Survey/SurveyRepository.php
+++ app/Repositories/Survey/SurveyRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repositories\Survey;
 
 use App\Models\Survey\Survey;
+use App\Models\Survey\SurveyQuestion;
 use App\Models\Survey\SurveyTemplate;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class SurveyRepository {
 
@@ -13,6 +15,19 @@ class SurveyRepository {
             $limit = $params['limit'] ?? 50;
             return DB::transaction(function () use ($limit) {
                 return Survey::query()->paginate($limit);
+            });
+        } catch (\Exception $e) {
+            throw new \Exception($e, 500);
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function getStockSurveys() {
+        try {
+            return DB::transaction(function () {
+                return Survey::query()->where('is_stock', '=', true)->paginate();
             });
         } catch (\Exception $e) {
             throw new \Exception($e, 500);
@@ -37,8 +52,8 @@ class SurveyRepository {
         return Survey::query()->findOrFail($surveys);
     }
 
-    public function getIfExist($surveys) {
-        return Survey::query()->find($surveys);
+    public function getIfExist($survey) {
+        return Survey::query()->find($survey);
     }
 
     public function getTemplate($survey_templates) {
@@ -82,12 +97,26 @@ class SurveyRepository {
         });
     }
 
-    public function delete(Survey $survey) {
+    public function destroy(Survey $survey) {
         return DB::transaction(function () use ($survey) {
             $survey->delete();
             return $survey;
         });
     }
 
+    private function storeSurveyQuestions(Survey $survey, array $questions): void {
+        foreach ($questions as $questionData) {
+
+            $question = new SurveyQuestion();
+
+            $question->title = $questionData['title'];
+            $question->is_required = $questionData['is_required'];
+            $question->question_type_id = $questionData['question_type_id'];
+            $question->survey_page_id = $questionData['survey_page_id'];
+            $question->additional_settings = $questionData['additional_settings'];
+
+            $question->save();
+        }
+    }
 
 }
