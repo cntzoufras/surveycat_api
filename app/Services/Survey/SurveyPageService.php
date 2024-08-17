b8ba172f820616e037ef98445c8caa0324c332b6
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-17 14:34:05 +0300

    Code style

--- app/Services/Survey/SurveyPageService.php
+++ app/Services/Survey/SurveyPageService.php
@@ -2,27 +2,26 @@
 
 namespace App\Services\Survey;
 
+use App\Models\Survey\SurveyPage;
 use App\Repositories\Survey\SurveyPageRepository;
 
 class SurveyPageService {
 
-    protected $survey_page_repository;
+    protected SurveyPageRepository $survey_page_repository;
 
     public function __construct(SurveyPageRepository $survey_page_repository) {
         $this->survey_page_repository = $survey_page_repository;
     }
 
     public function index(array $params) {
-
         return $this->survey_page_repository->index($params);
     }
 
-    public function store(array $params) {
+    public function store(array $params): SurveyPage {
         return $this->survey_page_repository->store($params);
     }
 
     public function update($survey_page, array $params) {
-
         $survey_page = $this->survey_page_repository->resolveModel($survey_page);
         return $this->survey_page_repository->update($survey_page, $params);
     }
@@ -32,7 +31,7 @@ class SurveyPageService {
         return $this->survey_page_repository->delete($survey_page);
     }
 
-    public function show($params) {
+    public function show($params): mixed {
         return $this->survey_page_repository->resolveModel($params);
     }
 
