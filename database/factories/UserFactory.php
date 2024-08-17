472d49844da795b61982918a93651cdfcb734f99
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-17 12:09:59 +0300

    Update User model photo to avatar

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory {
             'first_name'        => $this->faker->firstName,
             'last_name'         => $this->faker->lastName,
             'role'              => $this->faker->randomElement(['admin', 'premium', 'registered', 'guest']),
-            'photo'             => $this->faker->image,
+            'avatar'            => $this->faker->image,
             'email'             => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
