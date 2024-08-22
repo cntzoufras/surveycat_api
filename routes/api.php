3fd1b5d68b1c94b659bc7e9091e766d2a97a64fd
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-23 01:25:31 +0300

    Fix typo

--- routes/api.php
+++ routes/api.php
@@ -47,7 +47,7 @@ Route::group(['middleware' => [EnsureFrontendRequestsAreStateful::class, 'auth:s
     ]);
 
     Route::prefix('survey-questions')->group(function () {
-        Route::get('/types', [SurveyQuestionsController::class, 'getQuestionTypes']);
+        Route::get('/types', [SurveyQuestionController::class, 'getQuestionTypes']);
         Route::get('/', [SurveyQuestionController::class, 'index']);
         Route::post('/', [SurveyQuestionController::class, 'store']);
         Route::put('/{id}', [SurveyQuestionController::class, 'update']);
