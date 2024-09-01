295616c22fe6996e0fb70e71e7cf8676da91109a
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-09-01 18:47:50 +0300

    Update db seeder, keep users only local

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,9 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run(): void {
-        $this->call(UserSeeder::class);
+        if (config('app.env') === 'local') {
+            $this->call(UserSeeder::class);
+        }
         $this->call(ThemeSeeder::class);
         $this->call(ThemeSettingSeeder::class);
         $this->call(VariablePaletteSeeder::class);
