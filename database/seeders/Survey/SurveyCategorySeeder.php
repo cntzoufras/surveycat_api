69a68663379c094c0263541a1ee875fbabbb6a65
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-09-01 21:17:55 +0300

    Make SurveyCategory seeder have fixed data

--- database/seeders/Survey/SurveyCategorySeeder.php
+++ database/seeders/Survey/SurveyCategorySeeder.php
@@ -11,6 +11,34 @@ class SurveyCategorySeeder extends Seeder {
      * Run the database seeds.
      */
     public function run(): void {
-        SurveyCategory::factory()->count(68)->create();
+        // Get the path to the CSV file
+        $csvPath = base_path('database/seeds/survey_categories.csv');
+
+        // Open the CSV file
+        $file = fopen($csvPath, 'r');
+
+        // Skip the header row
+        fgetcsv($file);
+
+        // Loop through each row in the CSV file
+        while (($data = fgetcsv($file)) !== false) {
+            $survey_category = [$data[0], $data[1]];
+            $this->seedSurveyCategory($survey_category);
+        }
+
+        // Close the file
+        fclose($file);
+    }
+
+    /**
+     * Seed the question type to the database.
+     *
+     * @param array $survey_category
+     */
+    private function seedSurveyCategory(array $survey_category): void {
+        SurveyCategory::query()->create([
+            'title'       => $survey_category[0],
+            'description' => $survey_category[1],
+        ]);
     }
 }
