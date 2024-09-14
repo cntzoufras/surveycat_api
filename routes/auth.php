9078c531498f5b3fd2462b5def8e71a7f998957b
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-09-14 21:25:11 +0300

    Update auth urls to use /auth/ any

--- routes/auth.php
+++ routes/auth.php
@@ -8,25 +8,27 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-     ->middleware('guest')
-     ->name('password.email');
+Route::prefix('auth')->group(function () {
 
-Route::post('/reset-password', [NewPasswordController::class, 'store'])
-     ->middleware('guest')
-     ->name('password.store');
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+         ->middleware('guest')
+         ->name('password.email');
 
-Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-     ->middleware(['signed', 'throttle:6,1'])
-     ->name('verification.verify');
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])
+         ->middleware('guest')
+         ->name('password.store');
+
+    Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+         ->middleware(['signed', 'throttle:6,1'])
+         ->name('verification.verify');
 
 // User requests resend verification link
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-     ->middleware(['auth:sanctum', 'throttle:6,1'])
-     ->name('verification.send');
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+         ->middleware(['auth:sanctum', 'throttle:6,1'])
+         ->name('verification.send');
 
 //Route::middleware('auth:sanctum')->group(function () {
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-     ->name('web.logout');
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+         ->name('web.logout');
 //});
-
+});
