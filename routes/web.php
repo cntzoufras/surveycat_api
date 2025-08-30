c55f70aa3619da35dfc88f6f9190c1ca9b44a1d8
Constantinos Ntzoufras <cntzoufras@gmail.com>
2025-08-31 00:04:37 +0300

    Fix 422 post registration

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store']);
 
-Route::get('/sanctum/csrf-cookie', function () {
-    return response()->json(['csrfToken' => csrf_token()]);
-});
-
 require __DIR__ . '/auth.php';
